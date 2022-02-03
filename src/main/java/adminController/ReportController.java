286e2c35af4b710083a36f3176b4f9a36e09c2ab
ZahroNurrobbyAshari <zahronurrobbya@gmail.com>
2022-02-03 17:08:38 +0700

    fe clear

--- src/main/java/adminController/ReportController.java
+++ src/main/java/adminController/ReportController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package com.mycompany.cashnote;
+package adminController;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +13,7 @@ import javafx.fxml.Initializable;
  *
  * @author zahro
  */
-public class OutcomeController implements Initializable {
+public class ReportController implements Initializable {
 
     /**
      * Initializes the controller class.
